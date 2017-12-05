e4d6ee5b96ff31a3f3e667f7c585ff199d7d5ce0
Giancarlo Correa <gico.pilz@gmail.com>
2017-12-05 17:06:29 -0500

    Add files via upload

---
+++ Giftastic/assets/javascript/animalgifs.js
@@ -0,0 +1,20 @@
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apikey = "&api_key=mdBTTMPLgBDaFYls7i3XQIuzSglaQ8Rh";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).done(function(response) {
+}
+
+console.log(queryURL);
+
+console.log(response);
+
+
+
+
+
+
+var query = "&q=cats";
+var query = "&q=dogs";
+var query = "&q=birds";
